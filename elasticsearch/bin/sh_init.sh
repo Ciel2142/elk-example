983536a215f017e31fc41faf3b5676e79ce90461
= <igi2131@gmail.com>
2021-04-26 18:08:45 +0300

    open distro works

---
+++ elasticsearch/bin/sh_init.sh
@@ -0,0 +1,5 @@
+/usr/share/elasticsearch/plugins/opendistro_security/tools/securityadmin.sh -icl -nhnv \
+   -cd /usr/share/elasticsearch/plugins/opendistro_security/securityconfig/ \
+   -cacert /usr/share/elasticsearch/config/root-ca.pem \
+   -cert /usr/share/elasticsearch/config/kirk.pem \
+   -key /usr/share/elasticsearch/config/kirk-key.pem
